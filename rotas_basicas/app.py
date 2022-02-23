7f2a316be3347adc53253350a5a4a16b83fc5eab
serafimmatheus <matheus18serafim@gmail.com>
2022-02-23 12:26:14 -0300

    feat: work conclusion

--- rotas_basicas/app.py
+++ rotas_basicas/app.py
@@ -1,1 +1,27 @@
-# Seu código aqui
+from flask import Flask
+
+from http import HTTPStatus
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return {"data": "Hello Flask!"}, HTTPStatus.OK
+
+@app.get("/current_datetime")
+def current_datetime():
+    data = int(datetime.now().strftime("%H"))
+    data_formated = datetime.now().strftime("%d/%m/%Y %H:%M:%S %p")
+
+    if data < 12:
+         return {"current_datetime": data_formated, "message": "Bom Dia!"}, HTTPStatus.OK
+
+    elif data >= 12 and data < 18:
+         return {"current_datetime": data_formated, "message": "Boa Tarde!"}, HTTPStatus.OK
+
+    else:
+         return {"current_datetime": data_formated, "message": "Boa Noite!"}, HTTPStatus.OK
+
+
